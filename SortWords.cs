4d5d435c5bf4ee2347f73fc44bc3186bab006d42
srikanthpragada <srikanthpragada@gmail.com>
2019-07-31 09:32:10 +0530

    31-JUL

---
+++ SortWords.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace CsharpDemo
+{
+    class SortWords
+    {
+        static void Main(string[] args)
+        {
+            StreamReader sr = new StreamReader
+                 (@"e:\classroom\msnet\july10\text.txt");
+            var words = new SortedSet<string>();
+            
+            while (true)
+            {
+                string line = sr.ReadLine();
+                if (line == null)
+                    break;
+
+                string[] parts = line.Split(' ');
+                foreach (string w in parts)
+                    words.Add(w);
+
+            }
+            sr.Close();
+
+            foreach (string w in words)
+                Console.WriteLine(w);
+        }
+    }
+}
---
+++ SortedContacts.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CsharpDemo
+{
+    class SortedContacts
+    {
+        static void Main(string[] args)
+        {
+            var contacts = new SortedDictionary<string,string>();
+
+            while (true)
+            {
+                Console.Write("Enter name [end to stop] :");
+                string name = Console.ReadLine();
+                if (name == "end")
+                    break;
+                Console.Write("Enter mobile number :");
+                string mobile = Console.ReadLine();
+                contacts.Add(name, mobile);
+            }
+
+            foreach (string name in contacts.Keys)
+                Console.WriteLine("{0} - {1}", name, contacts[name]);
+        }
+    }
+}
